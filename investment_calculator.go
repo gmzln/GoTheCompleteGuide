117dfb72aa601a06472f545b0385c6b056d812e9
gmzln <gamze_ya@outlook.de>
2025-06-17 09:02:56 +0200

    finish small calculation app && add functions for outputting test and calculate the results

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,27 +5,24 @@ import (
 )
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
 
 	// fmt.Print("revenue: ")
-	outputText("revenue: ")
-	fmt.Scan(&revenue)
-	outputText("expenses: ")
-	fmt.Scan(&expenses)
-	outputText("taxrate: ")
-	fmt.Scan(&taxRate)
+	revenue := getUserInput("revenue: ")
+	expenses := getUserInput("expenses: ")
+	taxRate := getUserInput("taxrate: ")
 
 	ebt, profit, ratio := calculate(revenue, expenses, taxRate)
 	fmt.Printf("ebt: %.1f\nprofit: %v\nratio: %.1f\n ", ebt, profit, ratio)
 }
 
-func outputText(text string) {
-	fmt.Print(text)
+func getUserInput(outputText string) float64 {
+	var userInput float64
+	fmt.Print(outputText)
+	fmt.Scan(&userInput)
+	return userInput
 }
 
-// (values + type) (return types)
+// (accepted values + type) (return values + types)
 func calculate(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
